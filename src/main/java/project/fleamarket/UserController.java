bd891a884221c24020f1cf7a2da296d80cdac755
bronzehye0n <b911209@g.hongik.ac.kr>
2023-07-25 11:42:16 +0900

    컨트롤러 생성 및 기본 페이지 생성

---
+++ src/main/java/project/fleamarket/UserController.java
@@ -0,0 +1,21 @@
+package project.fleamarket;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping(value = "/user/login")
+    public String login(Model model) {
+        model.addAttribute("data", "/user/login");
+        return "login";
+    }
+
+    @GetMapping(value = "/user/signup")
+    public String signup(Model model) {
+        model.addAttribute("data", "/user/signup");
+        return "signup";
+    }
+}
